04f7aba3526e9684c6864596c11281b3a4d0a43e
bigmao8576 <bigmao8576@hotmail.com>
2019-12-24 01:53:32 -0500

    the utils

--- utils.py
+++ utils.py
@@ -7,9 +7,12 @@ Created on Tue Sep 10 20:36:45 2019
 """
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import KFold
+import os
+import random
 
-def file2dict(path,channel_num=2):
+def file2dict(path,channel_num=2,norm=True):
     '''
     This function is used for the second dataset
     Convert the csv data to dictionary
@@ -17,9 +20,12 @@ def file2dict(path,channel_num=2):
     input:
         path: the path of the csv file
         channel_num: the channel number of each sample
+        norm: If norm is Ture, each sample will be normalized accroding to it's own mean and std
     output:
         data_dict
         The length of all the samples
+        
+    
     '''
     data_frame=pd.read_csv(path, dtype={'Name':str, 'Value':float})
     data_list=data_frame.values.tolist()
@@ -27,9 +33,14 @@ def file2dict(path,channel_num=2):
     data_dict = {}
     for item in data_list:
         temp_data = np.array(item[1:])
-        data_dict[item[0]] = np.reshape(temp_data,[len(temp_data)//channel_num,channel_num])
+        if norm:
+            temp_data = (temp_data-np.mean(temp_data))/np.std(temp_data)
+            
+        length = len(temp_data)//channel_num
         
-    return data_dict, len(temp_data)//2
+        data_dict[item[0]] = np.reshape(temp_data,[length,channel_num])
+        
+    return data_dict, length
 
 def data2fold(data,fold = 5):
     name_list = data.keys()
@@ -61,4 +72,58 @@ def data2fold(data,fold = 5):
                               'test_data':test_data}
         tot_fold_name.append(fold_name)
     return fold_dict,tot_fold_name
+
+def data2array(data):
+    name_list = data.keys()
+    name_list = list(name_list)
+    
+    train_data = np.array([data[item] for item in name_list])
+    
+    return np.array(train_data),name_list
         
+def draw_pic_cost(plotdata,save_folder):
+
+    plt.plot(plotdata["epoch"],np.log10(np.array(plotdata["train_cost"])),'b') 
+    
+    try:
+        plt.plot(plotdata["epoch"],np.log10(np.array(plotdata["test_cost"])),'r')
+        plt.legend(['Training _set','Testing_set'])
+    except:
+        pass
+    
+    plt.xlabel('Epoch')
+    plt.ylabel('log10(cost)')
+    file_name = os.path.join(save_folder,'cost.png')
+    plt.savefig(file_name)
+    plt.close()
+    
+
+def draw_pic_sample(x_train,train_result_output,save_folder):
+    total_num = len(x_train)
+            
+    ind1 = random.randint(0,total_num-1)
+    ind2 = random.randint(0,total_num-1)
+    
+
+    plt.figure(3)
+    plt.plot(x_train[ind1,:,1]) 
+    plt.plot(train_result_output[ind1,:,1]) 
+    file_name = os.path.join(save_folder,'sampe1.png')
+    plt.savefig(file_name)  
+    plt.close()
+    
+    plt.figure(4)
+    plt.plot(x_train[ind2,:,1]) 
+    plt.plot(train_result_output[ind2,:,1]) 
+    file_name = os.path.join(save_folder,'sampe2.png')
+    plt.savefig(file_name)  
+    plt.close()
+
+
+def get_head(path):
+    data_frame=pd.read_csv(path, dtype={'Name':str, 'Value':float})
+    head_list = []
+    for col in data_frame.columns: 
+        head_list.append(col)
+    
+    return head_list[1:]
